d06fa9a547e7e6b0fec7a68cc79bc8f52530afe6
LogCreative <lizilong123456@live.cn>
2021-10-12 23:24:58 +0800

    project1 editDistanceDP

--- project1/msa_mdp.py
+++ project1/msa_mdp.py
@@ -12,6 +12,14 @@ def compare(c1,c2):
         return delta
     return 3        # mismatch
 
+def comparelist(cs):
+    # cyclic compare
+    res = 0
+    for i in range(len(cs)):
+        for j in range(i,len(cs)):
+            res += compare(cs[i],cs[j])
+    return res
+
 ####### Data Preprocessing #######
 
 curdir = os.path.dirname(__file__) + "/"
@@ -26,6 +34,12 @@ with open(curdir + "MSA_database.txt") as df:
 
 ####### MDP #######
 
+def visit(_list, _indices):
+    wrapper_ = _list
+    for _index in _indices:
+        wrapper_ = wrapper_[_index]
+    return wrapper_
+
 def editDistanceDP(S:list, dist = [], move= []):
     # Initialize l edges.
     # Example: 3 strings
@@ -38,7 +52,9 @@ def editDistanceDP(S:list, dist = [], move= []):
     for l in reversed(range(L)):
         for d in range(1 if l<L-1 else 0,len(S[l])+1):
             dist_core = d * delta
-            move_core = [(1 if i==l else 0) for i in range(L)]
+            move_core = ()
+            for i in range(L):
+                move_core = move_core + ((1 if i==l else 0),)
             for w in range(L-l-1):
                 dist_core = [dist_core]
                 move_core = [move_core]
@@ -54,6 +70,13 @@ def editDistanceDP(S:list, dist = [], move= []):
         index_order = [[d]+u 
             for d in range(0,len(S[l])+1) 
             for u in index_order]
+    # generate avaiable moves.
+    av_moves = [()]
+    for l in reversed(range(L)):
+        av_moves = [(d,)+u 
+            for d in range(0,2) 
+            for u in av_moves]
+    av_moves = av_moves[1:]
     # traverse through the index_order
     # we have to consider the edge case carefully.
     for indices in index_order:
@@ -66,8 +89,15 @@ def editDistanceDP(S:list, dist = [], move= []):
             else:
                 # This value is not calculated.
                 # calculate the new value.
-                dist_appender = 0
-                move_appender = [0,0,0]
+                moved_dict = {}
+                for av_move in av_moves:
+                    prev_node = [a-b for a,b in zip(indices, av_move)]
+                    if -1 in prev_node:
+                        continue        # move unaviable.
+                    prev_dist = visit(dist,prev_node)
+                    penalty = comparelist([S[s][c] if av_move[s]==1 else "-" for s,c in enumerate(prev_node)])
+                    moved_dict[av_move] = prev_dist + penalty
+                move_appender, dist_appender = min(moved_dict,key=moved_dict.get),min(moved_dict.values())
 
                 # extend the list.
                 for w in range(L-l):
@@ -78,4 +108,11 @@ def editDistanceDP(S:list, dist = [], move= []):
     print(dist)
     print(move)
 
-editDistanceDP(["AAAAA","BBBB","CCC"])
+def alignmentDP(S:list):
+    dist = []
+    move = []
+    cost = editDistanceDP(S,dist,move)
+
+    path = []
+
+editDistanceDP(["AAAAAA","BBBB","CCC"])
