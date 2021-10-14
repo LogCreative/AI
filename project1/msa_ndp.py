3736f24a11d441b11d79d7012f29b129128c6c56
LogCreative <lizilong123456@live.cn>
2021-10-14 22:06:07 +0800

    project1 dp report

--- project1/msa_ndp.py
+++ project1/msa_ndp.py
@@ -107,27 +107,27 @@ def alignmentDP(S):
                 S[axis] = S[axis][1:]
     return S_
 
-# print(alignmentDP(["AAAA","BBB"]))
+print(alignmentDP(["AAAA","BBB"]))
 
 # Cross check
-# with tqdm(total=len(pqs)*len(targets), desc="Starting Up", leave=True, unit='str') as pbar:
-#     with open(curdir + "ndp_pq.txt","w") as of:
-#         for pq in pqs:
-#             minindex = 0
-#             mincost = np.inf
-#             for d,tg in enumerate(targets):
-#                 pbar.set_description('Process: ' + pq[:10] + ' & ' + tg[:10])
-#                 S = [pq,tg]
-#                 dist,move = editDistanceDP(S)
-#                 fin = tuple(len(s) for s in S)
-#                 pcost = dist[fin]
-#                 if pcost < mincost:
-#                     minindex = d
-#                     mincost = pcost
-#                 pbar.update(1)
-#             of.write('\n'.join(alignmentDP([pq,targets[minindex]])))
-#             of.write('\n'+str(mincost)+"\n\n")
-#     pbar.set_description("Finish")
+with tqdm(total=len(pqs)*len(targets), desc="Starting Up", leave=True, unit='str') as pbar:
+    with open(curdir + "ndp_pq.txt","w") as of:
+        for pq in pqs:
+            minindex = 0
+            mincost = np.inf
+            for d,tg in enumerate(targets):
+                pbar.set_description('Process: ' + pq[:10] + ' & ' + tg[:10])
+                S = [pq,tg]
+                dist,move = editDistanceDP(S)
+                fin = tuple(len(s) for s in S)
+                pcost = dist[fin]
+                if pcost < mincost:
+                    minindex = d
+                    mincost = pcost
+                pbar.update(1)
+            of.write('\n'.join(alignmentDP([pq,targets[minindex]])))
+            of.write('\n'+str(mincost)+"\n\n")
+    pbar.set_description("Finish")
 
 with tqdm(total=len(mqs)*len(targets)*(len(targets)-1)/2, desc="Starting Up", leave=True, unit='str') as pbar:
     with open(curdir + "ndp_mq.txt","w") as of:
